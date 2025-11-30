ceece3dbd61525ddf511280632ce06a139710e4c
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-30 19:53:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_iam_user_group_membership" "user_membership" {
 resource "aws_iam_user_group_membership" "user_membership1" {
    user = var.user1
    groups = [
-        aws_iam_group.groups["var.group1].name
+        aws_iam_group.groups[var.group1].name
    ]
 }
 
