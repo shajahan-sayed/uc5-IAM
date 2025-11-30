dffbb4fcebbd646a64f6142a2f7bd50521a59535
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-30 20:09:20 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "created_users" {
   description = "List of IAM users created"
-  value       = aws_iam_user.users[*].name
+  value       = [for u in aws_iam_user.users : u.value.name]
 }
 
 output "created_groups" {
   description = "List of IAM groups created"
-  value       = aws_iam_group.groups[*].name
+  value       = [for g in aws_iam_group.groups : g.value.name]
 }
 
 output "user_group_assignment" {
